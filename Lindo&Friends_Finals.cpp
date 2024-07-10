bd55d3ad9409bfc1b8fd478df24fc967fca2ebdc
Laufeia <167260524+Laufeia@users.noreply.github.com>
2024-07-11 07:40:06 +0800

    Update Lindo&Friends_Finals.cpp

--- Lindo&Friends_Finals.cpp
+++ Lindo&Friends_Finals.cpp
@@ -1,6 +1,57 @@
-ABCD
-fgdfgdfgfdg
-fghfghfghfghfghfh
+#include<iostream>
+#include<iomanip>
+
+#include<string>
+
+using namespace std;
+
+class blank{
+protected:
+int choice;
+
+public:
+
+void mainMenu(){
+cout<<"Lindo & Friends"
+<<"[1] - Placeholder"
+<<"[2] - Placeholder"
+<<"[3] - Placeholder"
+<<"[4] - Placeholder"
+<<"[5] - Placeholder"
+<<"[6] - Placeholder";
+
+cin>>choice;
+
+}
+
+	
+};
 
 
-tangina niyo
+
+
+
+
+
+
+int main{
+	
+	mainMenu();
+	
+	switch(choice){
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+	}
+	
+	return 0;
+}
